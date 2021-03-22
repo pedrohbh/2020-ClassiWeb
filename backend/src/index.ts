b22411401ce9454a9d31f3f49291e44fc454cb66
VLRTroll <viniciusldr99@gmail.com>
2021-03-22 19:24:24 -0300

    Conexão com o Banco de Dados adicionada

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,32 @@
 import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
 import swaggerUi from 'swagger-ui-express';
+import { createConnection } from 'typeorm';
 
 import routes from './routes';
 import swaggerFile from './swagger_output.json';
 
-// Criação da API
-const app = express();
-
-// Habilita o CORS
-app.use(cors());
-
-// Configura leitura/resposta em JSON
-app.use(express.json());
-
-// Importa os endpoints da API
-app.use(routes);
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
 // Seleciona a porta da API
 export const port = 3333;
 
-app.listen(port);
+// Connects to the Database -> then starts the express
+createConnection()
+  .then(async (connection) => {
+    // Criação da API
+    const app = express();
+
+    // Configura middlewares
+    app.use(cors());
+    app.use(helmet());
+
+    // Configura leitura/resposta em JSON
+    app.use(express.json());
+
+    // Importa os endpoints da API
+    app.use(routes);
+    app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+    app.listen(port, () => console.log(`Server started on port ${port}!`));
+  })
+  .catch((error) => console.log(error));
