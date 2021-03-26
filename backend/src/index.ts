546364a231cf3b50c5e00ff072321d222f2fcc74
VLRTroll <viniciusldr99@gmail.com>
2021-03-26 18:18:46 -0300

    Base do backend substituido pelo framework Ts.ED

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,32 +1,17 @@
-import cors from 'cors';
-import express from 'express';
-import helmet from 'helmet';
-import swaggerUi from 'swagger-ui-express';
-import { createConnection } from 'typeorm';
+import {$log} from "@tsed/common";
+import { PlatformExpress } from "@tsed/platform-express";
+import {Server} from "./Server";
 
-import routes from './routes';
-import swaggerFile from './swagger_output.json';
+async function bootstrap() {
+  try {
+    $log.debug("Start server...");
+    const platform = await PlatformExpress.bootstrap(Server);
 
-// Seleciona a porta da API
-export const port = 3333;
+    await platform.listen();
+    $log.debug("Server initialized");
+  } catch (er) {
+    $log.error(er);
+  }
+}
 
-// Connects to the Database -> then starts the express
-createConnection()
-  .then(async (connection) => {
-    // Criação da API
-    const app = express();
-
-    // Configura middlewares
-    app.use(cors());
-    app.use(helmet());
-
-    // Configura leitura/resposta em JSON
-    app.use(express.json());
-
-    // Importa os endpoints da API
-    app.use(routes);
-    app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-    app.listen(port, () => console.log(`Server started on port ${port}!`));
-  })
-  .catch((error) => console.log(error));
+bootstrap();
