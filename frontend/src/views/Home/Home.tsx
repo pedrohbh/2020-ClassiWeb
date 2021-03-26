c5367bc9d6323096706a1bebaabe266e27e44601
Math-Gomes <mathjvmf@gmail.com>
2021-03-26 10:09:50 -0300

    Correções na home e footer adicionado

--- frontend/src/views/Home/Home.tsx
+++ frontend/src/views/Home/Home.tsx
@@ -1,73 +1,73 @@
 import { Grid } from '@material-ui/core';
 
 import AppBar from '../../components/AppBar';
+import Footer from '../../components/Footer';
+
 import Logotipo from '../../assets/ClassiWeb.svg';
 
 export default function Home() {
   return (
     <>
-      <AppBar/>
-      <Grid container style={{ marginTop: "70px" }}>
-        <Grid item xs={2} style={{ height: "max-content", border: "1px solid black" }}>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-          <div>Categoria 1</div>
-        </Grid>
-        <Grid item xs={10} style={{ height: "max-content", border: "1px solid black" }}>
-          <img src={Logotipo} alt="ClassiWeb" style={{ width: "100%", height: "25vh", border: "1px solid black"}} />
-          <div>Produto</div>
-          <div>Produto</div>
-          <div>Produto</div>
-          <div>Produto</div>
+    <Grid container direction="column" justify="flex-start"  style={{minHeight: "89vh"}}>
+
+      <Grid item xs={12}>
+        <AppBar/>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Grid container style={{ height:"100%", marginTop: "70px" }}>
+          
+          <Grid item xs={2} style={{ height: "max-content", boxShadow: "2px 2px 8px 1px #dedede", padding: "20px" }}>
+            <Grid container>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+              <Grid item xs={12}>Categoria 1</Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={10} style={{ height: "max-content" }}>
+            
+            <Grid item xs={12}>
+              <img src={Logotipo} alt="ClassiWeb" style={{ width: "100%", height: "25vh" }} />
+            </Grid>
+          
+            <Grid container item xs={12} style={{ padding: "20px"}}>
+              <Grid item xs={12}>Produto</Grid>
+              <Grid item xs={12}>Produto</Grid>
+              <Grid item xs={12}>Produto</Grid>
+              <Grid item xs={12}>Produto</Grid>
+              <Grid item xs={12}>Produto</Grid>
+              <Grid item xs={12}>Produto</Grid>
+              <Grid item xs={12}>Produto</Grid>
+              <Grid item xs={12}>Produto</Grid>
+              <Grid item xs={12}>Produto</Grid>
+              <Grid item xs={12}>Produto</Grid>
+              <Grid item xs={12}>Produto</Grid>
+              <Grid item xs={12}>Produto</Grid>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
+      
+    </Grid>
+    <Footer/>
     </>
   );
 }
