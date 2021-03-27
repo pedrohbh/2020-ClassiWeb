d68c0185f57f823c3faab29617a63a56331144ae
allanfermao <allanfermao@gmail.com>
2021-03-27 13:54:15 -0300

    tela login iniciada

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './views/Home/Home'
 import Register from './views/Register/Register'
+import Login from './views/Login/Login'
 
 export default function Routes() {
     return (
@@ -10,6 +11,7 @@ export default function Routes() {
             <Switch>
                 <Route path='/' component={Home} />
                 <Route path='/register' component={Register} />
+                <Route path='/login' component={Login} />
             </Switch>
         </BrowserRouter>
     );
